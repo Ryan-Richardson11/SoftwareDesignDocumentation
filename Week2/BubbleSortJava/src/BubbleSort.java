d3e3826ed4a68ade8c084e0e44a1ba636d01cce3
unknown <ryanrich811@gmail.com>
2023-09-07 10:32:59 -0400

    Added java documentation to program

--- Week2/BubbleSortJava/src/BubbleSort.java
+++ Week2/BubbleSortJava/src/BubbleSort.java
@@ -1,7 +1,16 @@
 import java.util.Arrays;
 
+/**
+ * BubbleSort class, contains a bubblesort method for a given array.
+ */
 public class BubbleSort {
 
+    /**
+     * swap method, takes two elements in an array and swaps them.
+     * @param A An inputted array.
+     * @param i Index of the first element being swapped.
+     * @param j Index of the second element being swapped.
+     */
     public static void swap(int[] A, int i, int j) {
         int temp = A[i];
         A[i] = A[j];
@@ -9,6 +18,10 @@ public class BubbleSort {
 
     }
 
+    /**
+     * bubbleSort method, performs bubble sort algorithm on a given array.
+     * @param A An array of integers.
+     */
     public static void bubbleSort(int[] A) {
         for (int i = 0; i < A.length - 1; i++) {
             for (int j = 0; j < A.length - i - 1; j++) {
@@ -19,6 +32,11 @@ public class BubbleSort {
         }
     }
 
+    /**
+     * main method, creates an array of integers and passes it as an arguemnt to bubbleSort method.
+     * Sorted array is printed out.
+     * @param args Unused commnad line argument.
+     */
     public static void main(String[] args) {
         int[] myArray = { 2, 45, 37, 21, 31, 50, 29, 22, 67, 88, 56 };
         bubbleSort(myArray);
