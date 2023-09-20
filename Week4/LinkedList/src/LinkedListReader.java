52d1092b9ac32b1affb554d6f72408c5024c8b97
Ryan Richardson <ryanrich811@gmail.com>
2023-09-20 08:52:00 -0400

    Completed code for reading values and sorting a linkedList

--- Week4/LinkedList/src/LinkedListReader.java
+++ Week4/LinkedList/src/LinkedListReader.java
@@ -1,21 +1,25 @@
 import java.util.LinkedList;
-import java.util.ListIterator;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class LinkedListReader {
 
-    void addAll(String allValues, LinkedList<String>) {
-        LinkedList splitString = allValues.split(" ");
+    void addAll(String allValues, LinkedList<Integer> linkedList) {
+        String[] splitString = allValues.split(" ");
         for (int i = 0; i < splitString.length; i++) {
-            add(splitString[i]);
+            linkedList.add(Integer.parseInt(splitString[i]));
         }
+        Collections.sort(linkedList);
     }
 
     public static void main(String[] args) {
-        LinkedList<String> list = new LinkedList<>();
-        ListIterator<String> it = list.listIterator();
+        LinkedList<Integer> list = new LinkedList<>();
         Scanner input = new Scanner(System.in);
         System.out.println("Enter integers followed by a space (Ex: 1 4 3 9): ");
         String ans = input.nextLine();
+        LinkedListReader reader = new LinkedListReader();
+        reader.addAll(ans, list);
+        input.close();
+        System.out.println(list);
     }
 }
