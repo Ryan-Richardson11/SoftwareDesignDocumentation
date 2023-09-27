6941f976d7f7278b0cfcf6dc671dda4dce5fd202
Ryan Richardson <ryanrich811@gmail.com>
2023-09-27 19:43:10 -0400

    All instances of linkedList Swapped with collections stack

--- Week5/RichardsonrjP05/src/StackReader.java
+++ Week5/RichardsonrjP05/src/StackReader.java
@@ -1,11 +1,11 @@
-import java.util.LinkedList;
+import java.util.Stack;
 import java.util.Scanner;
 import java.util.Collections;
 
 /**
  * LinkedListReader class, contains methods to read an input for a linked list.
  */
-public class LinkedListReader {
+public class StackReader {
 
     /**
      * addAll method, takes an input as a parameter and converts it to a sorted
@@ -14,21 +14,21 @@ public class LinkedListReader {
      * @param allValues  Values inputted by the user
      * @param linkedList linked list created from java collections.
      */
-    void addAll(String allValues, LinkedList<Integer> linkedList) {
+    void addAll(String allValues, Stack<Integer> stack) {
         String[] splitString = allValues.split(" ");
         for (int i = 0; i < splitString.length; i++) {
-            linkedList.add(Integer.parseInt(splitString[i]));
+            stack.add(Integer.parseInt(splitString[i]));
         }
         // Sorts linked list.
-        Collections.sort(linkedList);
+        Collections.sort(stack);
     }
 
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
+        Stack<Integer> list = new Stack<>();
         Scanner input = new Scanner(System.in);
         System.out.println("Enter integers followed by a space (Ex: 1 4 3 9): ");
         String ans = input.nextLine();
-        LinkedListReader reader = new LinkedListReader();
+        StackReader reader = new StackReader();
         try {
             reader.addAll(ans, list);
             input.close();
